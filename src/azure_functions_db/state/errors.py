8a2d5263372fbb81d2bd6879c5bbbb5517fdda6b
Yeongseon Choe <yeongseon.choe@gmail.com>
2026-04-08 05:08:07 +0000

    style: fix import sorting (ruff I001)

--- src/azure_functions_db/state/errors.py
+++ src/azure_functions_db/state/errors.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from ..core.errors import DbError
 
+
 class StateStoreError(DbError):
     pass
 
--- src/azure_functions_db/trigger/errors.py
+++ src/azure_functions_db/trigger/errors.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from ..core.errors import DbError
 
+
 class PollerError(DbError):
     pass
 
